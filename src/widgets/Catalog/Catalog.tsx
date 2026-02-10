b05fcf349ffd968c765cff44c76615d5617eb499
Andrey <katniss1111@yandex.ru>
2026-02-10 23:36:04 +0500

    10.02Fix

--- src/widgets/Catalog/Catalog.tsx
+++ src/widgets/Catalog/Catalog.tsx
@@ -23,39 +23,40 @@ export default function Catalog() {
 
   if (productsData) {
     return (
-      <div className={style.container}>
-        <div className={style.containerButtons}>
-          <ButtonFilterOpen
-            name={CatalogConstants.FiltersButtons[0].name}
-            svg={CatalogConstants.FiltersButtons[0].svg}
-            handleClick={handleClickFilterButton}
-          />
-          <ButtonFilterOpen
-            name={CatalogConstants.FiltersButtons[1].name}
-            svg={CatalogConstants.FiltersButtons[1].svg}
-            handleClick={handleClickFilterButton}
-          />
-        </div>
-        <div>
-          <h2 className={style.title}>{CatalogConstants.CatalogInfo.title}</h2>
-          <p className={style.items}>
-            {products === null || products === undefined
-              ? CatalogConstants.CatalogInfo.error
-              : filteredProducts.length === 0
-                ? `${filteredProducts.length} ${CatalogConstants.CatalogInfo.products} ${CatalogConstants.CatalogInfo.nullFilterProducts}`
-                : `${filteredProducts.length} ${CatalogConstants.CatalogInfo.products}`}
-          </p>
-        </div>
-        <div className={style.productsContainer}>
-          {productsData.map((product) => (
-            <div key={product.id}>
-              <CardProduct product={product} />
-            </div>
-          ))}
-        </div>
+      <div className={style.catalog}>
+        <div className={style.container}>
+          <div className={style.containerButtons}>
+            <ButtonFilterOpen
+              name={CatalogConstants.FiltersButtons[0].name}
+              svg={CatalogConstants.FiltersButtons[0].svg}
+              handleClick={handleClickFilterButton}
+            />
+            <ButtonFilterOpen
+              name={CatalogConstants.FiltersButtons[1].name}
+              svg={CatalogConstants.FiltersButtons[1].svg}
+              handleClick={handleClickFilterButton}
+            />
+          </div>
+          <div>
+            <h2 className={style.title}>{CatalogConstants.CatalogInfo.title}</h2>
+            <p className={style.items}>
+              {products === null || products === undefined
+                ? CatalogConstants.CatalogInfo.error
+                : filteredProducts.length === 0
+                  ? `${filteredProducts.length} ${CatalogConstants.CatalogInfo.products} ${CatalogConstants.CatalogInfo.nullFilterProducts}`
+                  : `${filteredProducts.length} ${CatalogConstants.CatalogInfo.products}`}
+            </p>
+          </div>
+          <div className={style.productsContainer}>
+            {productsData.map((product) => (
+              <div key={product.id}>
+                <CardProduct product={product} />
+              </div>
+            ))}
+          </div>
 
-        <Pagination pagination={pagination} />
-
+          <Pagination pagination={pagination} />
+        </div>
         <FullModal
           title={modalContent}
           isOpen={isOpen}
